5e303e1203c90024cbfb7d3f9acd55cab75f0826
Edu Renesto <edure95@gmail.com>
2018-09-12 18:35:55 -0300

    screenshot with xrectsel

--- bin/screenshot.sh
+++ bin/screenshot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-scrot /tmp/screenshot.png -e 'xclip -sel clipboard -t image/png < $f; rm $f'
+scrot /tmp/screenshot.png -e 'convert $f -crop `xrectsel` $f; xclip -sel clipboard -t image/png < $f; rm $f'
